1cd1f9ba5e35c5da34647bcf25b1c19c72d41efc
05701265684 <adriano.andrade@ufop.edu.br>
2021-04-16 23:31:43 -0300

    incluido exercicio ate o 4

--- main.cpp
+++ main.cpp
@@ -102,12 +102,13 @@ void inicializaVetorRandom(int v[], int tam) {
     std::cout << " " << endl;
 }
 
-void inicializaVectorRandom(vector<int> &nums, int tam) {
+std::vector<int> inicializaVectorRandom(vector<int> &nums, int t) {
     unsigned seed = time(0);
     srand(seed);
-    for (int i = 0; i < tam; i++) {
+    for (int i = 0; i < t; i++) {
         nums.push_back(1 + rand() % 1000);
     }
+    return nums;
 }
 
 
@@ -131,11 +132,6 @@ void nPermute(string str, long int n) {
     cout << str;
 }
 
-// C++ program to print all
-// permutations with duplicates allowed
-#include <bits/stdc++.h>
-
-
 vector<vector<int>> lexic(int n) {
     vector<vector<int>> permutations;
     vector<int> permutation;
@@ -158,7 +154,7 @@ vector<vector<int>> lexic(int n) {
         if (!dir) {
             int j = i;
 
-            for (int k = i; k < n ; k++) {
+            for (int k = i; k < n; k++) {
                 if (permutation[i] < permutation[k]) { // O (n * n! )
                     j = k;
                 }
@@ -186,13 +182,13 @@ void printmatriz(vector<vector<int>> &permutacao) {
 // 1. String
 // 2. Starting index of the string
 // 3. Ending index of the string.
-void lexicographicPermute(  string str, int inicio, int fim) {
+void lexicographicPermute(string str, int inicio, int fim) {
     std::vector<int> v;
-    for(int i=1; i< fim+1; i++){
+    for (int i = 1; i < fim + 1; i++) {
         v.push_back(i);
     }
 
-   // string str; int inicio; int fim;
+    // string str; int inicio; int fim;
     if (inicio == fim)
         cout << str << endl;
     else {
@@ -215,86 +211,230 @@ void lexicographicPermute(  string str, int inicio, int fim) {
 }
 
 
-void lexicographicPermute11(  vector<int> v, int inicio, int fim ) {
-    if (inicio == fim)
-        printVector(v);
-        //cout << str << endl;
-    else {
-        for (int i = inicio; i <= fim; i++) {
-//            int aux = v[inicio];
-//            v[inicio] = v[i];
-//            v[i] = aux;
-            swap(v[inicio], v[i]);
+void print_permutations(std::vector<std::vector<int>> &permutations) {
 
-            // Recursion called
-            lexicographicPermute11(v, inicio + 1, fim);
-
-            //backtrack
-
-//            aux = v[inicio];
-//            v[inicio] = v[i];
-//            v[i] = aux;
-            swap(v[inicio], v[i]);
+    for (std::vector<int> p : permutations) {
+        for (int el : p) {
+            std::cout << el << " ";
         }
+        std::cout << std::endl;
     }
+
 }
 
-void lexicographicPermute1( int n ) {
-    int inicio=0; int fim=n;
+std::vector<std::vector<int>> lexic_permute(int n) {
+    std::vector<std::vector<int>> permutations;
+    std::vector<int> permutation;
+
+    for (int i = 1; i < n + 1; i++) {
+        permutation.push_back(i);
+    }
+
+    permutations.push_back(permutation);
+
+    bool decreasing_order = false;
+
+    do {
+        int i = -1;
+
+        for (int k = 0; k < n - 1; k++) {
+            if (permutation[k] < permutation[k + 1]) {
+                i = k;
+            }
+        }
+
+        decreasing_order = (i == -1);
+
+        if (!decreasing_order) {
+            int j = i;
+
+            for (int k = i; k < n; k++) {
+                if (permutation[i] < permutation[k]) {
+                    j = k;
+                }
+            }
+
+            std::swap(permutation[i], permutation[j]);
+
+            std::reverse(permutation.begin() + (i + 1), permutation.end());
+
+            permutations.push_back(permutation);
+        }
+
+    } while (!decreasing_order);
+
+    print_permutations(permutations);
+    return permutations;
+}
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+
+//  O( n * n!)
+std::vector<vector<int>> johnTrotter(std::vector<int> &array, bool *direcao, int k, std::vector<vector<int>> mv) {
+    for (int i = 1; i < k; i++) {
+        int n = array.size();
+        int maiorMovel = 0;
+        int pegaPosicaoMaiorMovel = 0;
+
+        //   encontrar o maior  inteiro móvel.
+        for (int i = 0; i < n; i++) {
+            // maiorMovel direita para array esquerda
+            if (direcao[array[i] - 1] == false && i != 0) { //  n= 1 e 2
+                if ((array[i] > array[i - 1]) && (array[i] > maiorMovel)) {
+                    maiorMovel = array[i];
+                }
+            }
+
+            // array direção 1 representa da ESQUERDA PARA A DIREITA.
+            if ((direcao[array[i] - 1] == true) && (i != n - 1)) {
+                if ((array[i] > array[i + 1]) && (array[i] > maiorMovel)) {
+                    maiorMovel = array[i];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (array[i] == maiorMovel)
+                pegaPosicaoMaiorMovel = i + 1; //pega array posução do elemento, antes da mudança de maiorMovel
+        }
+
+        // trocando os elementos de acordo com array direção, ou seja, direcao [].
+        if (direcao[array[pegaPosicaoMaiorMovel - 1] - 1] == false) {
+            swap(array[pegaPosicaoMaiorMovel - 1], array[pegaPosicaoMaiorMovel - 2]);
+        } else if (direcao[array[pegaPosicaoMaiorMovel - 1] - 1] == true) {
+            swap(array[pegaPosicaoMaiorMovel], array[pegaPosicaoMaiorMovel - 1]);
+        }
+
+        // alterando as direções dos elementos maior que o maior número inteiro móvel.
+        for (int i = 0; i < n; i++) {
+            if (array[i] > maiorMovel) {
+                if (direcao[array[i] - 1] == true)
+                    direcao[array[i] - 1] = false;
+                else if (direcao[array[i] - 1] == false)
+                    direcao[array[i] - 1] = true;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            cout << array[i];
+        }
+        cout << endl;
+        mv.push_back(array);
+    }
+
+    return mv;
+}
+
+
+int factorial(int n) {
+    if (n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
+
+void inicializaJohn(int n) {
+
     std::vector<int> v;
+    std::vector<vector<int>> mv;
+    bool direcao[n];
 
-    for(int i=1; i< n+1; i++){
-        v.push_back(i);
+    int k = factorial(n);
+    cout << "Número de permutação: " << k << endl;
+
+    for (int i = 0; i < n; i++) {
+        v.push_back(i + 1);
+        cout << v[i];
     }
 
-    // string str; int inicio; int fim;
-    if (inicio == fim)
-        printVector(v);
-        //cout << str << endl;
-    else {
-        // Permutations made
-        for (int i = inicio; i <= fim; i++) {
-            int aux = v[inicio];
-            v[inicio] = v[i];
-            v[i] = aux;
+    cout << endl;
 
-            // Recursion called
-            lexicographicPermute11(v, inicio + 1, fim);
-
-            //backtrack
-            // swap(str[inicio], str[i]);
-            aux = v[inicio];
-            v[inicio] = v[i];
-            v[i] = aux;
-        }
+    for (int i = 0; i < n; i++) {
+        direcao[i] = false;
     }
+
+    mv.push_back(v);
+    johnTrotter(v, direcao, k, mv);
 }
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
+// ------------------------------ johnTrotter ------------------------------------------------------------------------
 
 int main() {
-    /*
 
-    //  1 ------- INSERTION SORT -----------------------
-    inicializaVectorRandom(nums,tam_v);
 
-   cout<<"Vector antes da ordenação: \n";
-   printVector(nums);
+    //  11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    //  --------------------------------------------------------------------------------------------------------------
+    //  --------------------------------------------------------------------------------------------------------------
+    //  ---------------------- INSERTION SORT ------------------------------------------------------------------------
+    //  ---------------------- INSERTION SORT ------------------------------------------------------------------------
+    //  --------------------------------------------------------------------------------------------------------------
+    //  --------------------------------------------------------------------------------------------------------------
 
-   cout<<"Vector depois da ordenação: \n";
-   printVector(insertSort(nums));
-   */
+//    int tam_v=20;
+//    std::vector<int> nums;
+//    std::vector<int> numsV = inicializaVectorRandom(nums,tam_v);
+//
+//    cout << "Vector antes da ordenação: \n";
+//    printVector(numsV);
+//
+//    cout << "Vector depois da ordenação: \n";
+//    numsV=insertSort(nums);
+//    printVector(numsV );
 
-// funciona
+
+
+    //222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------- Johnson-Trotter -------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    //222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+    //222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+
+    // int n = 3;
+    // inicializaJohn(n);
+
+
+
+    // 33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+    // 33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+    // -----------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------lexicographicPermute---------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
+
+    //    std::vector<int> ns({3});
+//    //  std::vector<int> ns({ 2,4,5,6,7,9,10});
+//    std::vector<double> time({});
+//
+//    for (int n : ns) {
+//        auto start = std::chrono::system_clock::now();
+//        lexic_permute(n);
+//        auto finish = std::chrono::system_clock::now();
+//
+//        std::chrono::duration<double> elapsed = finish - start;
+//        std::cout << "N = " << n << " : " << elapsed.count() << std::endl;
+//
+//        time.push_back(elapsed.count());
+//    }
+
+
+    //444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    //444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+    // -----------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------
+
 //    string str;
 //    cout << "Insira a sequencia de string: ";
 //    getline(cin, str);
-  //  int n = str.size();
-  //  lexicographicPermute(str, 0, n - 1);
-
-    // 2 ----------------------------------------------------------------------------------
-    // --------------------- lexicographicPermute1 --------------------------------------
-    // ----------------------------------------------------------------------------------
-    lexicographicPermute1(3);
-
+//    int n = str.size();
+//    lexicographicPermute(str, 0, n - 1);
     return 0;
 
 }
