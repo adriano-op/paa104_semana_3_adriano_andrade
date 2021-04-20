f49091d3917836149f60742b7c8dec78366c710c
05701265684 <adriano.andrade@ufop.edu.br>
2021-04-20 18:56:28 -0300

    incluido exercicio ate o 9, excceto o 8 e o matplot

--- main.cpp
+++ main.cpp
@@ -771,8 +771,38 @@ int quickSelect(int *arr, int l, int r, int k)
     // elementos na matriz
     return INT_MAX;
 }
+// generates all subsets
+void generate(int k, vector<bool> & my_set, int N)
+{
+    if(k == N)
+    {
+        // shows the subset
+        cout << "{ ";
+        for(int i = 0; i < N; i++)
+        {
+            if(my_set[i] == true)
+                cout << i + 1 << " ";
+        }
+        cout << "}\n";
+    }
+    else
+    {
+        my_set[k] = true;
+        generate(k + 1, my_set, N);
+        my_set[k] = false; // backtracking
+        generate(k + 1, my_set, N);
+    }
+}
 
 int main() {
+    int N;
+
+    cin >> N;
+
+    vector<bool> my_set(N);
+
+    generate(0, my_set, N);
+
     //https://www.programmersought.com/article/88824602188/
 
 
